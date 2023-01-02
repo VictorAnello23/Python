d63b68d5ebd1bb07512f810100a3376009e4747f
VictorAnello23 <121406579+VictorAnello23@users.noreply.github.com>
2023-01-02 01:13:55 -0300

    Add files via upload

---
+++ cuar.py
@@ -0,0 +1,21 @@
+Usuario_ = "VictorAnello"
+Contraseña_ = "Android09"
+print("Bienvenido al programa!")
+print("Usuario: ")
+Usuario = input()
+print("Contraseña: ")
+Contraseña = input()
+if Usuario == Usuario_ and Contraseña_ == Contraseña :
+    print("Has entrado crack!")
+else: 
+    print("Has fallado, vuelve más tarde...")
+print("Quieres saber tu signo?: ")
+Si = "1"
+No = "2"
+print("1-Si")
+print("2-No")
+res = input()
+if Si:
+    print ("En que mes naciste?")
+else:
+    print("Adiós!")
